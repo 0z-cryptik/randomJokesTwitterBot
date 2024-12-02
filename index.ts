7bb3258be855d3f1bfeed7943e0bb580859ef406
Eniitan <enitanolawale0@gmail.com>
2024-12-02 22:40:43 +0100

    setup the foundations

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('badoo')
+import { config } from "dotenv";
+import { twitterClient } from "./twitter_client";
+
+config();
+
+const tweet = async () => {
+  try {
+    await twitterClient.v2.tweet("Hello world");
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+tweet();
