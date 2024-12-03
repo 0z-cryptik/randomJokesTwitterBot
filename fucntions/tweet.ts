202e51d34f7487a417563bca1a3fb888ba329bc4
Eniitan <enitanolawale0@gmail.com>
2024-12-03 01:52:43 +0100

    debugging

--- fucntions/tweet.ts
+++ fucntions/tweet.ts
@@ -3,6 +3,7 @@ import { twitterClient } from "../twitterStuff/twitter_client.js";
 export const tweet = async (text: string) => {
   try {
     await twitterClient.v2.tweet(text);
+    console.log(`posted ${text}`);
   } catch (e) {
     console.error(e);
   }
